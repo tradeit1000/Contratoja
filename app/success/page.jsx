c56a81f7f11d151903db15ac866c6ebd4abdecce
Joao Aires <Joao@MacBookPro.lan>
2026-03-24 03:18:54 +0000

    force dynamic

--- app/success/page.jsx
+++ app/success/page.jsx
@@ -1,5 +1,2 @@
-"use client";
-import dynamic from "next/dynamic";
-export const dynamicPage = "force-dynamic";
-const C = dynamic(() => import("../../components/SuccessContent"), { ssr: false });
-export default function SuccessPage() { return <C />; }
+export const dynamic = "force-dynamic";
+export { default } from "../../components/SuccessContent";
